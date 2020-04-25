ef9dee9f8fdf04e45ec86fe307eb7e406d1f9afa
Vladimir Josan <vladimir.josan@ebs-integrator.com>
2020-04-25 16:08:59 +0300

    added dublicate checker

--- src/createMessages/createMessages.tsx
+++ src/createMessages/createMessages.tsx
@@ -1,4 +1,4 @@
-import { throwError } from '../throw';
+import { throwError, throwErrorLog } from '../throw';
 
 export interface Messages {
   [key: string]: {
@@ -10,10 +10,18 @@ export function createMessages($messages: Messages[]): Messages {
   let messages = {} as Messages;
 
   if (!$messages || ($messages && !Array.isArray($messages))) {
-    throwError(`expect array, but got ${typeof $messages}`);
+    throwError(`createMessages expects array, but got ${typeof $messages}`);
   }
 
   $messages.forEach((message) => {
+    const [messagesKeys, messageKeys] = [Object.keys(messages), Object.keys(message)];
+
+    messageKeys.forEach((messageKey) => {
+      if (messagesKeys.includes(messageKey)) {
+        throwErrorLog(`createMessages found key "${messageKey}" two or more times`);
+      }
+    });
+
     messages = { ...messages, ...message };
   });
 
